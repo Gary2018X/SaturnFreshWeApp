bf68d290138f72bbc51c656fcaff3b6961134654
Gary <56428019+Gary2018X@users.noreply.github.com>
2021-04-10 14:23:57 +0800

    优化login

--- pages/auth/login/login.js
+++ pages/auth/login/login.js
@@ -10,10 +10,16 @@ var user = require('../../../utils/user.js');
 var app = getApp();
 Page({
   data: {
-
+    userInfo: {},
+    hasUserInfo: false,
+    canIUseGetUserProfile: false,
   },
   onLoad: function (options) {
-
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true
+      })
+    }
   },
   onReady: function () {
 
@@ -37,21 +43,43 @@ Page({
     // })
 
   },
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
+    // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+        this.login()
+      }
+    })
+  },
+  getUserInfo(e) {
+    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+    this.login()
+  },
   login: function (e) {
     let that = this;
-    // console.log(e.detail)
     wx.login({
       success(res) {
         util.request(api.getSessionKeyByCode, {
           code: res.code,
-          user_info:e.detail.userInfo
+          user_info:that.data.userInfo
         }, 'POST').then(res => {
           if (res.code === 200) {
             wx.setStorageSync('openId', res.data.openid);
             that.setData({
               sessionKey: res.data.session_key
             })
-            wx.setStorageSync('userInfo', e.detail.userInfo);
+            wx.setStorageSync('userInfo',that.data.userInfo);
+            console.log(that.data.userInfo)
             wx.setStorageSync('token', res.data.session_key);
             app.globalData.hasLogin = true;
             app.globalData.tabBarCartNum = res.data.cartGoodsCount;
