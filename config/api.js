fd9d39364bfd2f5e2115f88d26911701c45ee4af
Gary <56428019+Gary2018X@users.noreply.github.com>
2021-04-10 11:34:46 +0800

    不需要本地运行后端

--- config/api.js
+++ config/api.js
@@ -1,8 +1,8 @@
 // 以下是业务服务器API地址
-// 本机开发时使用
-// var WxApiRoot = 'http://192.168.0.3:8080/wx/';
+// 本机开发时使用真机调试
+var WxApiRoot = 'http://39.102.36.135:8080/wx/';
 // 局域网测试使用
-var WxApiRoot = 'http://localhost:8000/wx/';
+// var WxApiRoot = 'http://localhost:8000/wx/';
 // 云平台部署时使用
 // var WxApiRoot = 'http://wx-api.freemanzhao.top/';
 // 云平台上线时使用
